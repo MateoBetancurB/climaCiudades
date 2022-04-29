4988584ae439a7ff23b902aef74f8225f2729cab
Mateo <mateobetancurb@gmail.com>
2022-04-29 09:41:18 -0500

    mostrar información del API en HTML

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ function buscarClima(e) {
 		mostrarError("Ambos campos son obligatorios");
 		return;
 	}
-	// console.log(ciudad);
-	// console.log(pais);
+
+	//consultar la API
+	consultarAPI(ciudad, pais);
 }
 
 //mostrar errores
@@ -53,5 +54,54 @@ function mostrarError(mensaje) {
 		setTimeout(() => {
 			alerta.remove();
 		}, 3000);
+	}
+}
+
+//extraer datos del API
+function consultarAPI(ciudad, pais) {
+	const appId = "26f17521532f9513c76bb1bb6f5967c1";
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+	fetch(url)
+		.then((respuesta) => {
+			return respuesta.json();
+		})
+		.then((datos) => {
+			limpiarHTML(); //limpiar HTML previo
+			if (datos.cod === "404") {
+				mostrarError("Ciudad no encontrada");
+				return;
+			}
+
+			//imprimir respuesta en HTML
+			mostrarClima(datos);
+		});
+}
+
+function mostrarClima(datos) {
+	const {
+		main: { temp, temp_max, temp_min },
+	} = datos;
+	const centigrados = kelvinACentigrados(temp);
+
+	const actual = document.createElement("p");
+	actual.innerHTML = `Clima actual: ${centigrados} &#8451`;
+	actual.classList.add("font-bold", "text-5xl");
+
+	const resultadoDiv = document.createElement("div");
+	resultadoDiv.classList.add("text-center", "text-white");
+	resultadoDiv.appendChild(actual);
+
+	resultado.appendChild(resultadoDiv);
+}
+
+//mostrar la medición en HTML como grados centígrados
+function kelvinACentigrados(grados) {
+	return parseInt(grados - 273.15);
+}
+
+function limpiarHTML() {
+	while (resultado.firstChild) {
+		resultado.removeChild(resultado.firstChild);
 	}
 }
